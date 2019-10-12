46fbf7770d14281089b5ec19754467637f2c1605
Jesse Arstein <jesse.arstein@yahoo.com>
2019-10-11 21:41:21 -0600

    Implemented RDT_3_0

--- RDT.py
+++ RDT.py
@@ -57,7 +57,7 @@ class RDT:
     ## latest sequence number used in a packet
     seq_num = 1
     ## buffer of bytes read from network
-    byte_buffer = '' 
+    byte_buffer = ''
 
     def __init__(self, role_S, server_S, port):
         self.network = Network.NetworkLayer(role_S, server_S, port)
@@ -111,9 +111,9 @@ class RDT:
                         self.byte_buffer = self.byte_buffer[length:]
                         self.network.udt_send(p.get_byte_S()) #If not resend.
                     else:
-                        rec_pkt = Packet.from_byte_S(self.byte_buffer[0:length])
+                        receivedPacket = Packet.from_byte_S(self.byte_buffer[0:length])
                         self.byte_buffer = self.byte_buffer[length:]
-                        if(rec_pkt.msg_S == 'ACK' and rec_pkt.seq_num >= self.seq_num):#Check if ACK packet.
+                        if(receivedPacket.msg_S == 'ACK' and receivedPacket.seq_num >= self.seq_num):#Check if ACK packet.
                             self.seq_num = self.seq_num + 1
                             return
                         else:
@@ -149,10 +149,10 @@ class RDT:
                 self.byte_buffer = self.byte_buffer[length:]
                     
     def waitForMore(self, ack): #Method for making sure there is no resends. Wait for .1 seconds
-        end_time = time.time() + .1
+        end = time.time() + .1
         byte_buffer2 = ''
-        while (time.time() < end_time):
-            was_duplicate = False
+        while (time.time() < end):
+            isDuplicate = False
             bytes2 = self.network.udt_receive()
             byte_buffer2 += bytes2
 
@@ -166,14 +166,14 @@ class RDT:
                 nack = Packet(self.seq_num, 'NACK') #Create NACK packet.
                 self.network.udt_send(nack.get_byte_S()) #Send
                 byte_buffer2 = '' #Empty the buffer.
-                if (was_duplicate): #Checks for duplicates and adds more time
-                    end_time = end_time + .1
+                if (isDuplicate): #Checks for duplicates and adds more time
+                    end = end + .1
                 continue
             else: # Time expired
                 p2 = Packet.from_byte_S(byte_buffer2[0:length])
                 if (p2.seq_num == self.seq_num - 1): #Check if it was a different packet.
-                    was_duplicate = True
-                    end_time = end_time + .1
+                    isDuplicate = True
+                    end = end + .1
                     self.network.udt_send(ack.get_byte_S()) #We don't have to wait anymore send ACK.
                     byte_buffer2 = ''
                 else:
@@ -183,10 +183,93 @@ class RDT:
 
 
     def rdt_3_0_send(self, msg_S):
-        pass
-        
+        p = Packet(self.seq_num, msg_S)
+        self.network.udt_send(p.get_byte_S())
+
+        byte_S = self.network.udt_receive()
+        self.byte_buffer += byte_S
+
+        while True: # Keep checking for packets.
+            timeoutTime = time.time() + .05 #Timer for lost packets.
+            while(time.time() < timeoutTime):
+                byte_S = self.network.udt_receive()
+                self.byte_buffer += byte_S
+                if(len(self.byte_buffer) >= Packet.length_S_length):
+                    length = int(self.byte_buffer[:Packet.length_S_length])
+                    if(len(self.byte_buffer) >= length): #Make sure packet is right length.
+                        if(Packet.corrupt(self.byte_buffer[0:length])): #Check to make sure not corrupt.
+                            self.byte_buffer = self.byte_buffer[length:]
+                            break
+                        else:
+                            receivedPacket = Packet.from_byte_S(self.byte_buffer[0:length])
+                            self.byte_buffer = self.byte_buffer[length:]
+                            if(receivedPacket.msg_S == 'ACK' and receivedPacket.seq_num >= self.seq_num): #Check if right ACK packet for one we sent.
+                                self.seq_num = self.seq_num + 1
+                                self.byte_buffer = self.byte_buffer[length:]
+                                return
+                            else:
+                                break #Break out the timer somethings wrong.
+            self.network.udt_send(p.get_byte_S())
+
+
     def rdt_3_0_receive(self):
-        pass
+        ret_S = None
+        byte_S = self.network.udt_receive()
+        self.byte_buffer += byte_S
+        while True: #Keep checking for packets.
+            if (len(self.byte_buffer) < Packet.length_S_length): #Is packet right length
+                return ret_S
+            length = int(self.byte_buffer[:Packet.length_S_length])
+            if (len(self.byte_buffer) < length):
+                return ret_S
+            if(Packet.corrupt(self.byte_buffer[0:length])): # Check for corrupt packets.
+                nack = Packet(self.seq_num, 'NACK')
+                self.network.udt_send(nack.get_byte_S())
+                self.byte_buffer = self.byte_buffer[length:]
+            else:
+                p = Packet.from_byte_S(self.byte_buffer[0:length])
+                if (p.seq_num <= self.seq_num): #Is packet right sequence number.
+                    ret_S = p.msg_S if (ret_S is None) else ret_S + p.msg_S
+                    self.seq_num = self.seq_num + 1
+                    ack = Packet(p.seq_num, 'ACK')
+                    self.network.udt_send(ack.get_byte_S())
+                    end = time.time() + .2
+                    byte_buffer2 = ''
+                    while(time.time() < end):
+                        isDuplicate = False
+                        bytes2 = self.network.udt_receive()
+                        byte_buffer2 += bytes2
+                        try:
+                            if (len(byte_buffer2) < Packet.length_S_length):
+                                continue
+                        except ValueError:
+                            continue
+                        length = int(byte_buffer2[:Packet.length_S_length])
+                        if (len(byte_buffer2) < length):
+                            continue
+                        if(Packet.corrupt(byte_buffer2[0:length])):
+                            nack = Packet(self.seq_num, 'NACK')
+                            self.network.udt_send(nack.get_byte_S())
+                            byte_buffer2 = ''
+                            if(isDuplicate):
+                                end = end + .2
+                            continue
+                        else:
+                            p2 = Packet.from_byte_S(byte_buffer2[0:length])
+                            if (p2.seq_num <= self.seq_num-1):
+                                isDuplicate = True
+                                end = end + .2
+                                ack1 = Packet(p2.seq_num, 'ACK')
+                                self.network.udt_send(ack1.get_byte_S())
+                                byte_buffer2 = ''
+                            else:
+                                nack = Packet(self.seq_num, 'NACK')
+                                self.network.udt_send(nack.get_byte_S())
+                                break
+                else:
+                    nack = Packet(self.seq_num, 'NACK')
+                    self.network.udt_send(nack.get_byte_S())
+                self.byte_buffer = self.byte_buffer[length:]
 
 
 if __name__ == '__main__':
